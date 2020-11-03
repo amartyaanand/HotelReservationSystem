987d5fd34b44bf7e65f11239822bb4d657ce5a65
Amartya <amartyaanand21@live.com>
2020-11-03 23:19:59 +0530

    [Amartya] Add : Adding Hotel in HotelReservation System.

---
+++ HotelReservationSystem/HotelType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public enum HotelType
+    {
+        LAKEWOOD,
+        BRIDGEWOOD,
+        RIDGEWOOD
+    }
+}
