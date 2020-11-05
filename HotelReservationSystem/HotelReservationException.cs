af0799717b4ccdf134afdb70140981199d04d391
Amartya <amartyaanand21@live.com>
2020-11-05 13:12:09 +0530

    [Amartya] Add : Add special rates for reward customers as layalty program.

--- HotelReservationSystem/HotelReservationException.cs
+++ HotelReservationSystem/HotelReservationException.cs
@@ -8,7 +8,9 @@ namespace HotelReservationSystem
     {
         public enum ExceptionType
         {
-            INVALID_HOTEL_TYPE
+            INVALID_HOTEL_TYPE,
+            INVALID_DATE,
+            INVALID_CUSTOMER_TYPE
         }
         ExceptionType type;
         public HotelReservationException(ExceptionType type, string message) : base(message)
